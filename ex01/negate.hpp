7d4a9ced5cfbbd00a0d470cf23f4cb699d243e2b
nponchon <nponchon@42.com>
2025-03-09 20:52:06 +0100

    ex01 uses any function

--- ex01/negate.hpp
+++ ex01/negate.hpp
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
-#include <cstddef>
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   negate.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/09 19:22:41 by nponchon          #+#    #+#             */
-/*   Updated: 2025/03/09 19:26:04 by nponchon         ###   ########.fr       */
+/*   Created: 2025/03/09 20:44:26 by nponchon          #+#    #+#             */
+/*   Updated: 2025/03/09 20:47:51 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <cstddef>
-
-void negate(int &n)
+template <typename T>
+void negate(T &t)
 {
-    n = -n;
-}
-
-template <typename T, typename Func> void iter(T* array, size_t len, Func f)
-{
-	for (size_t i = 0; i < len; i++)
-		f(array[i]);
+	t = -t;
 }
